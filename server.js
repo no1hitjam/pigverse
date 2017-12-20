71211711e9e509576bfe0e66b3034638a25811c5
Jackson Lango <jacksonlango@gmail.com>
2017-12-19 19:50:08 -0800

    Add redis test to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
+var redis = require('redis');
 
 const PORT = process.env.PORT || 3000;
 
@@ -29,3 +30,11 @@ function broadcast(data)
     	client.send(data);
   	});
 }
+
+
+var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+
+client.set('foo', 'bar');
+client.get('foo', function (err, reply) {
+    console.log(reply.toString()); // Will print `bar`
+});
